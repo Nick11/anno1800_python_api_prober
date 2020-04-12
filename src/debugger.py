e24cd41bbdd3f1765ba093036af17fad11529ac1
nick11 <sniclas@ethz.ch>
2020-04-12 18:33:42 +0200

    Trying to figure out API

--- src/debugger.py
+++ src/debugger.py
@@ -25,17 +25,72 @@ def main():
 	#data = DATA['TextSources'].TextSourceRoots.GetTradeRoute().GetRoute(10)
 	#data = DATA['scenes'].SessionTradeRouteOverview.TradeRouteOverviewObject.RouteData[0].RouteID
 	#data = DATA['scenes'].SessionTradeRoutes.Data.StationData[0].StationName
-	#data = DATA['rdsdk::CRDString']('TEST')
-	data = api.getTradeRoutes(DATA)
-	#data = dir(data)
+	routeIDs = getTradeRouteIDs(DATA)
+	routeID = routeIDs[0]
+	routes = getTradeRoutes(DATA)
+	route = routes[3]
+	data = route.MouseLeft()
+	
+	data = DATA['scenes'].SessionTradeRouteOverview.TradeRouteOverviewObject.RouteData[0]
+	data = DATA['scenes'].SessionTradeRouteOverview.TradeRouteOverviewObject.RouteData[1]
+	
+	
+	#openRoute(DATA)
+	modules['scenes'].SessionTradeRouteOverview.TradeRouteOverviewObject.RouteData[0]
+	
+	
+	#data = DATA['scenes'].SessionTradeRoutes.Data.StationData[1].StationID
+	
+	
+	
+	#data = DATA['TextSources'].TextSourceRoots.GetTradeRoute().GetRoute(routeID).GetStation(0)
+	#data = DATA['TextSources'].TextSourceRoots.TradeRoute.GetRoute(routeID)
+	#data = DATA['scenes'].ObjectMenuShip.FleetObject.ActiveItemData
 	#data = len(data)
 	#data = vars(data)
 	log(data)
+	log(dir(data))
+	log(len(data))
+	log('\n')
 
 def log(text):
 	with open(LOG_FILE, 'a') as log_file:
 		log_file.write(str(text)+'\n\n')
 
+def getTradeRoutes(modules):
+	return scenes(modules).SessionTradeRouteOverview.TradeRouteOverviewObject.RouteData
+	
+def getTradeRouteIDs(modules):
+	routeData = getTradeRoutes(modules)
+	return [route.RouteID for route in iter(routeData)]
 
+def getAreasForRoute(modules, route):
+	pass
+
+def getAreaFromId(modules, areaID):
+	return TextSourceRoots(modules).GetArea().GetAreaFromID(areaID)
+
+def getStationsForTradeRoute(tradeRouteID):
+	pass
+	
+def TextSourceRoots(modules):
+	return modules['TextSources'].TextSourceRoots
+
+
+def scenes(modules):
+	return modules['scenes']
+	
+def iter(self):
+	for i in range(len(self)):
+		yield self[i]
+
+# Notes
+def openCharterRoute(modules):
+	modules['scenes'].SessionTradeRouteOverview.TradeRouteOverviewObject.TransferData[1].RouteButtonClicked()
+def openRoute(modules):
+	modules['scenes'].SessionTradeRouteOverview.TradeRouteOverviewObject.RouteData[0].EditRouteClicked()
+def getCargoForLAstSelectedShip():
+	DATA['scenes'].ObjectMenuShip.ShipObject.CargoSlotData[0].ItemData.ButtonBehaviour.RefGUID
+	
 main()
 
